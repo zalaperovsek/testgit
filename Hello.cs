ce275545b5429651f41d688ad105cf9ecf874aaf
zalaperovsek <46672641+zalaperovsek@users.noreply.github.com>
2019-01-14 03:01:28 -0800

    Update Hello.cs

--- Hello.cs
+++ Hello.cs
@@ -1,8 +1,9 @@
-# // Ime datoteke: Hello.cs
+// Ime datoteke: Hello.cs
 public class Hello
 {
    public static void Main()
    {
-      System.Console.WriteLine("Hello, World!");
+	System.Console.WriteLine("Hello, World!");
+	System.Console.WriteLine("Hello, GitHub!");
    }
 }
